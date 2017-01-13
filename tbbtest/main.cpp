597a47027710effc7560fc6ec06d3ebf8f0e6f2f
Nicolas Guillemot <nlguillemot@gmail.com>
2017-01-12 22:00:41 -0800

    cleanup, docs and, bugfixes

--- tbbtest/main.cpp
+++ tbbtest/main.cpp
@@ -5,14 +5,17 @@
 #include <cassert>
 #include <atomic>
 
+// node in a linked list of tasks that depend on a cont
 struct cont_node
 {
     tbb::task* task;
     cont_node* next;
 };
 
+// base class for working with conts (encapsulates tricky atomic code)
 class cont_base
 {
+    // head of the linked list of successors queued on this cont
     std::atomic<cont_node*> _head = NULL;
 
 public:
@@ -23,18 +26,21 @@ public:
     cont_base(cont_base&&) = delete;
     cont_base& operator=(cont_base&&) = delete;
 
+    // return true if this cont has been set_ready()
     bool is_ready() const
     {
-        return ((intptr_t)_head.load() & 1) != 0;
+        // TODO: pick better than memory_order_seq_cst?
+        return ((intptr_t)_head.load(std::memory_order_seq_cst) & 1) != 0;
     }
 
+    // sends this cont to all successors in the linked list.
     void set_ready()
     {
         assert(!is_ready());
 
         cont_node* old_head;
 
-        // mark the cont as ready atomically
+        // mark the cont as ready atomically. readiness is indicated by the least significant bit of the head pointer.
         for (;;)
         {
             // TODO: pick better than memory_order_seq_cst
@@ -45,7 +51,7 @@ public:
             if (_head.compare_exchange_weak(old_head, ready_head, std::memory_order_seq_cst))
             {
                 // If the CAS failed, that means a successor just added themselves to the list,
-                // since that successor thought this cont was not ready yet.
+                // since that successor thought this cont was not ready yet. (or possibly it was a spurious wakeup.)
                 // If it succeeded, then the cont can notify all successors that have been queued so far.
                 break;
             }
@@ -56,17 +62,15 @@ public:
         {
             if (node->task->decrement_ref_count() == 0)
             {
-                tbb::task* parent = (*node->task).parent();
-                if (parent)
-                {
-                    parent->add_ref_count(1);
-                }
-
+                // this was the last missing input, so the task can now be spawned.
                 tbb::task::spawn(*node->task);
             }
         }
     }
 
+    // Tries adding the given task to the cont's successor linked list using the given linked list node.
+    // This fails (and returns false) if the successor queue has already been closed because the cont has already been set.
+    // If it succeeds (and returns true), then the passed-in task was successfully added to the linked list.
     bool try_register_successor(tbb::task* t, cont_node* c)
     {
         cont_node* new_head = c;
@@ -76,11 +80,12 @@ public:
         cont_node* old_head = _head.load(std::memory_order_seq_cst);
         new_head->next = old_head;
 
-        // TODO: pick better than memory_order_seq_cst
+        // TODO: pick better than memory_order_seq_cst and consider if I should use _explicit
         return _head.compare_exchange_strong(old_head, new_head, std::memory_order_seq_cst);
     }
 };
 
+// Associates data to a cont, std::optional-style.
 // TODO: Just replace all of this with std::optional?
 template<class T>
 class cont : public cont_base
@@ -140,34 +145,33 @@ public:
     }
 };
 
+// spawns the given task when all the "conts" are ready. There must be a linked list node supplied for each cont.
 void spawn_when_ready(tbb::task& t, cont_base** conts, cont_node* nodes, int num_conts)
 {
+    // +1 reference count for each missing argument
+    // the task is only spawned when the reference count is zero,
+    // so that means it gets decremented once for each input that gets filled in.
     t.add_ref_count(num_conts);
 
-    bool any_missing = false;
+    int num_inputs_already_ok = 0;
 
     for (size_t cont_i = 0; cont_i < num_conts; cont_i++)
     {
         cont_base* c = conts[cont_i];
-        
+
+        // try registering the task as a successor of each cont, so the task will get notified (and its refcount decremented) when the cont becomes available
         if (!c->try_register_successor(&t, &nodes[cont_i]))
         {
-            t.decrement_ref_count();
-        }
-        else
-        {
-            any_missing = true;
+            // if we can't subscribe a successor to the cont, that means the cont is already set.
+            // in other words, that input is already ready to go, and we don't need to wait for a notification about it.
+            num_inputs_already_ok++;
         }
     }
     
-    if (!any_missing)
+    // incorporate the inputs that already okay into the reference count.
+    // if the reference count hits zero, that means all inputs are satisfied and the task can be spawned.
+    if (t.add_ref_count(-num_inputs_already_ok) == 0)
     {
-        tbb::task* parent = t.parent();
-        if (parent)
-        {
-            parent->add_ref_count(1);
-        }
-        
         tbb::task::spawn(t);
     }
 }
@@ -188,8 +192,11 @@ class TaskA : public tbb::task
         {
             std::cout << "A_Subtask1\n";
 
+            // set the value of c
             c->emplace(1337);
+            // broadcast that c is ready to all the successors enqueued on the cont.
             c->set_ready();
+
             return NULL;
         }
     };
@@ -217,9 +224,12 @@ public:
     {
         std::cout << "A\n";
 
+        // 2 children + wait
         set_ref_count(3);
+
         spawn(*new(allocate_child()) A_Subtask1(c));
         spawn_and_wait_for_all(*new(allocate_child()) A_Subtask2());
+
         return NULL;
     }
 };
@@ -254,6 +264,7 @@ public:
         std::stringstream ss;
         ss << "C received " << **c << "\n";
         std::cout << ss.rdbuf();
+
         return NULL;
     }
 };
@@ -271,11 +282,12 @@ public:
         spawn(*new(allocate_child()) TaskA(&c));
         spawn(*new(allocate_child()) TaskB());
 
+        // run TaskC when all the conts are satisfied (cont "c" gets set inside TaskA)
         TaskC& taskC = *new(allocate_child()) TaskC(&c);
         cont_base* conts[] = { &c };
         spawn_when_ready(taskC, conts, taskC.cnodes, _countof(conts));
 
-        spawn_and_wait_for_all(*new (allocate_child()) tbb::empty_task());
+        wait_for_all();
 
         return NULL;
     }
